557ff6968a6aac52255fa5841b89729ffb593b51
Lambda-256 <6ec0meaher0@gmail.com>
2025-09-04 15:42:49 +0900

    MOD概要の修正

---
+++ src/main/java/com/MissFrom/AshenMod/main/AshenMod.java
@@ -0,0 +1,13 @@
+package com.MissFrom.AshenMod.main;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod("ashenmod")
+public class AshenMod {
+
+    public static final String MOD_ID = "ashenmod";
+
+    public AshenMod(){
+
+    }
+}
