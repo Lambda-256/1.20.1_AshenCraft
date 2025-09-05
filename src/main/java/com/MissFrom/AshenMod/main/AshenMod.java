1eb7e163d6797db93d15ac19bbef13c8b8b97e7d
Seutem <house027653@gmail.com>
2025-09-05 13:59:39 +0900

    メインクラスがStatusクラスを呼び出すように変更

--- src/main/java/com/MissFrom/AshenMod/main/AshenMod.java
+++ src/main/java/com/MissFrom/AshenMod/main/AshenMod.java
@@ -1,5 +1,6 @@
 package com.MissFrom.AshenMod.main;
 
+import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
 
 @Mod("ashenmod")
@@ -8,6 +9,6 @@ public class AshenMod {
     public static final String MOD_ID = "ashenmod";
 
     public AshenMod(){
-
+        MinecraftForge.EVENT_BUS.register(Status.class);
     }
 }
