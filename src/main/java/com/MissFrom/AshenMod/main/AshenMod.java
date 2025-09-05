b896cad019fd5413719d5b96f07dc449e427b2a2
Lambda-256 <6ec0meaher0@gmail.com>
2025-09-05 15:01:44 +0900

    プレイヤーレベルをmainから呼び出し

--- src/main/java/com/MissFrom/AshenMod/main/AshenMod.java
+++ src/main/java/com/MissFrom/AshenMod/main/AshenMod.java
@@ -1,6 +1,8 @@
 package com.MissFrom.AshenMod.main;
 
+import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
+import com.MissFrom.AshenMod.main.capability.CapabilityEventHandler;
 
 @Mod("ashenmod")
 public class AshenMod {
@@ -8,6 +10,7 @@ public class AshenMod {
     public static final String MOD_ID = "ashenmod";
 
     public AshenMod(){
-
+        // Capabilityイベントを登録
+        MinecraftForge.EVENT_BUS.register(CapabilityEventHandler.class);
     }
 }
